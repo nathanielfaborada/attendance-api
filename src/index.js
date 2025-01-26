f6db4db8a70ecba2f2b62bac014d5cf92997e5eb
Nathaniel Faborada <faboradanathaniel@gmail.com>
2025-01-27 00:13:31 +0800

    attendance

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config.js';
+import cors from 'cors';
+
+import v1 from './routes/v1/index.js';
+import './core/database.js';
+import morgan from 'morgan';
+
+const app = express();
+const port = process.env.PORT || 8080
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/v1', cors() ,v1);
+
+app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`))
